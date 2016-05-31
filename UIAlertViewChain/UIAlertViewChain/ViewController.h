be8e96a24ec251e7d8a44a3f90c1450311ecfa8c
hj_wu <hj_wu@ctrip.com>
2016-05-31 19:50:00 +0800

    UIAlertViewChain init

---
+++ UIAlertViewChain/UIAlertViewChain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAlertViewChain
+//
+//  Created by whj on 16/5/31.
+//  Copyright © 2016年 whj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIAlertViewChain/UIAlertViewChain/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIAlertViewChain
+//
+//  Created by whj on 16/5/31.
+//  Copyright © 2016年 whj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
