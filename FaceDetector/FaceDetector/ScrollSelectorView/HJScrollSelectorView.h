3c8894305fbc7273f2077c665ae6c6f101001501
hj_wu <hj_wu@ctrip.com>
2016-11-16 15:35:45 +0800

    使用说明:

--- FaceDetector/FaceDetector/ScrollSelectorView/HJScrollSelectorView.h
+++ FaceDetector/FaceDetector/ScrollSelectorView/HJScrollSelectorView.h
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 dream. All rights reserved.
 //
 
+
+
 #import <UIKit/UIKit.h>
 
 typedef enum : NSUInteger {
@@ -15,6 +17,27 @@ typedef enum : NSUInteger {
 } ScrollSelectorViewOrientation;
 
 typedef void(^TouchedAction)(id sender);
+
+
+/**
+ 使用说明:
+ 0.导入文件夹ScrollSelectorView;
+ 1.#import "HJScrollSelectorView.h";
+ 2.@property (nonatomic, strong) HJScrollSelectorView *scrollSelectorView;
+ 3.self.scrollSelectorView = [[HJScrollSelectorView alloc] initWithFrame:CGRectMake(0, 0, 320, 150)];
+ [self.scrollSelectorView resource:@[@"icon-film", @"icon_photo"]];
+ [self.scrollSelectorView registerCellName:@"HJImageTableViewCell"];
+ [self.scrollSelectorView setCellHeight:150];
+ [self.scrollSelectorView cellOrientation:Orientation_Left];
+ [self.view addSubview:self.scrollSelectorView];
+ 
+ __weak typeof(self) weakSelf = self;
+ [self.scrollSelectorView cellTouchedWithBlock:^(id sender) {
+ 
+ __weak typeof(weakSelf) strongSelf = weakSelf;
+ [strongSelf changeStyle:[sender isEqualToString:@"icon_photo"]];
+ }];
+ */
 
 @interface HJScrollSelectorView : UIView
 
